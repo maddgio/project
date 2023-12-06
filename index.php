5c90c3d81d673750704fcd8fb6c91091e99b2a50
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-12-05 20:58:44 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,8 @@ include "view-header.php";
 <p>This is a database for a bus transportation company.</p> <br/>
 <p>Tab 1 (Drivers): Drivers and the buses they drive (with schedule)</p>
 <p>Tab 2 (Buses): Buses and their working schedule</p>
-<p>Tab 3 (Drivers with Shifts): Divers and their shifts along with the route they take during each shift</p>
-<p>Tab 4 (Employees Weekly Pay): The amount of the hours each employee works per week and the pay calculation</p>
-<p>Tab 5 (Routes Driven Before Refueling): Calculating the amount of routes a driver can take before needing to refuel</p>
-<p>Tab 6 (Route Map): Shows the map of an overview of the stops on the routes</p><br/>
+<p>Tab 3 (Drivers Shifts and Weekly Pay): Divers and their shifts along with the pay for each week</p>
+<p>Tab 5 (Routes Driven Before Refueling): Calculating the number of routes a driver can take before needing to refuel</p>
 <p>This database uses the tables: Drivers, Buses, Shifts, and Routes.</p>
 <?php
 include "view-footer.php";
