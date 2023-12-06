4449fe93d5a7609f6a8f9c39052e424ee9997bf6
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-12-05 22:16:16 -0600

    Update view-drivers.php

--- view-drivers.php
+++ view-drivers.php
@@ -52,3 +52,68 @@
     </tbody>
   </table>
 </div>
+
+<h1>Bus Driver Weekly Pay Calculator</h1>
+
+    <label for="employeeId">Enter Employee ID:</label>
+    <input type="text" id="employeeId" placeholder="Enter employee ID">
+    <br>
+    <button onclick="fillHours()">Fill Hours</button>
+    <br>
+    <label for="hoursWorked">Enter Hours Worked:</label>
+    <input type="number" id="hoursWorked" placeholder="Enter hours">
+    <button onclick="calculatePay()">Calculate Weekly Pay</button>
+
+    <p id="result"></p>
+
+    <script>
+        // Store hours worked for each employee
+        const employeeHours = {
+            '1': 8,
+            '2': 11,
+            '3': 6,
+            '4': 17,
+            '7': 5,
+            // Add more employees if needed
+        };
+
+        function fillHours() {
+            const employeeIdInput = document.getElementById('employeeId');
+            const hoursWorkedInput = document.getElementById('hoursWorked');
+
+            const employeeId = employeeIdInput.value.trim();
+
+            if (employeeHours[employeeId]) {
+                hoursWorkedInput.value = employeeHours[employeeId];
+            } else {
+                alert("No predefined hours for the entered Employee ID.");
+            }
+        }
+
+function calculateWeeklyPay(hoursWorked, employeeId) {
+            const hourlyRate = 16; // Default rate
+
+            // Check if predefined hours exist for the employee
+            const predefinedHours = employeeHours[employeeId];
+            const actualHours = predefinedHours || hoursWorked;
+
+            return actualHours * hourlyRate;
+        }
+
+        function calculatePay() {
+            const employeeIdInput = document.getElementById('employeeId');
+            const hoursWorkedInput = document.getElementById('hoursWorked');
+            const resultParagraph = document.getElementById('result');
+
+            const employeeId = employeeIdInput.value.trim();
+            const hoursWorked = parseFloat(hoursWorkedInput.value);
+
+            // Check if both inputs are valid
+            if (employeeId === '' || isNaN(hoursWorked)) {
+                resultParagraph.textContent = "Please enter valid information.";
+            } else {
+                const weeklyPay = calculateWeeklyPay(hoursWorked, employeeId);
+                resultParagraph.textContent = `Weekly pay for Employee ${employeeId}: $${weeklyPay.toFixed(2)}`;
+            }
+        }
+    </script>
